e7522f64641fa5b1b0c9d086c7117e8b8bdded15
Aurghyadip Kundu <adkundu@gmail.com>
2018-04-04 18:27:19 +0530

    Add project files.

---
+++ XamarinRecyclerTest/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using Android.Support.V7.Widget;
+using System.Json;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.IO;
+
+namespace XamarinRecyclerTest
+{
+    [Activity(Label = "Xamarin Application", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        private RecyclerView recyclerView;
+        private RecyclerView.LayoutManager mlayoutManager;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            JsonValue jsonValue;
+            mlayoutManager = new LinearLayoutManager(this);
+
+            Button button = FindViewById<Button>(Resource.Id.fetch_json);
+
+            button.Click += async (sender, e) => {
+
+                // Get the latitude and longitude entered by the user and create a query.
+                string url = "http://jsonplaceholder.typicode.com/posts";
+
+                // Fetch the weather information asynchronously, 
+                // parse the results, then update the screen:
+                JsonValue json = await FetchPostsAsync(url);
+                // ParseAndDisplay (json);
+            };
+
+
+        }
+
+        private async Task<JsonValue> FetchPostsAsync(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    JsonValue jsonValue = await Task.Run(() => JsonValue.Load(stream));
+                    Console.Out.WriteLine(jsonValue.ToString());
+
+                    return jsonValue;
+                }
+            }
+        } 
+    }
+}
+
---
+++ XamarinRecyclerTest/PostAdapter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.Widget;
+
+namespace XamarinRecyclerTest
+{
+    class PostAdapter : RecyclerView.Adapter
+    {
+        private List<Post> posts;
+
+        public PostAdapter(List<Post> posts)
+        {
+            this.posts = posts;
+        }
+
+        public override int ItemCount { get => posts.Count; }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            PostViewHolder viewHolder = holder as PostViewHolder;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.CardView, parent, false);
+            PostViewHolder viewHolder = new PostViewHolder(itemView);
+            return viewHolder;
+        }
+
+
+        public class PostViewHolder : RecyclerView.ViewHolder
+        {
+
+            private TextView userIdView;
+            private TextView postIdView;
+            private TextView postTitleView;
+            private TextView postBodyView;
+
+            public PostViewHolder(View itemView) : base(itemView)
+            {
+                userIdView = itemView.FindViewById<TextView>(Resource.Id.user_id_field);
+                postIdView = itemView.FindViewById<TextView>(Resource.Id.post_id_field);
+                postTitleView = itemView.FindViewById<TextView>(Resource.Id.post_title_field);
+                postBodyView = itemView.FindViewById<TextView>(Resource.Id.post_body_field);
+            }
+
+            public void SetUserIdView(int userId)
+            {
+                userIdView.Text = userId.ToString();
+            }
+
+            public void SetPostIdView(int postId)
+            {
+                postIdView.Text = postId.ToString();
+            }
+
+            public void SetPostTitleView(string postTitle)
+            {
+                postTitleView.Text = postTitle;
+            }
+
+            public void SetPostBodyView(string postBody)
+            {
+                postBodyView.Text = postBody;
+            }
+        }
+    }
+}
